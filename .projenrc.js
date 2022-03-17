b17e53a87495f782ceddd234e4a4e30f15083c2e
Ayman Zahran <ah.zahran@outlook.com>
2022-03-17 13:03:54 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,20 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Ayman Zahran',
+  authorAddress: 'ah.zahran@outlook.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'ProjenTest',
+  repositoryUrl: 'https://github.com/AymanZahran/ProjenTest.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+
+  python: {
+    distName: 'acme.hello-jsii',
+    module: 'acme.hello_jsii',
+  },
+});
+project.synth();
